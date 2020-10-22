e4332d2b48f793166e65219a9d3fb52d9a3cbd75
shaundawson <iamsdawson@gmail.comgit config --global color.ui truegit config --global core.editor emacsssh-keygen -t rsa -C iamsdawson@gmail.compbcopy  ~/.ssh/id_rsa.pubgit config --global user.email iamsdawson@gmail.com>
2020-10-22 18:49:41 -0400

    fix env file

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
-require('dotenv').config();
+
+require('dotenv').config(); // Loads environment variables from a .env file and adds them to the process.env object that is made available to the application
 const express = require('express'); // Imports express framework
 const logger = require('morgan'); // Express middleware for logging requests and responses. Useful during dev mode so you can see what requests are being made.
 const bodyParser = require('body-parser'); // Express middleware that you should use with forms. Adds a body object into your requuests so you can access POST parameter.
 const session = require('express-session');
 const path = require('path');
+const reactViews     = require('express-react-views');
 const methodOverride = require('method-override');
 const cookieParser = require('cookie-parser');
 const movieRouter = require('./routes/moviesRouter');
@@ -22,7 +24,7 @@ app.engine('jsx', require('express-react-views').createEngine());
 
 app.use(logger('dev')); // Set up morgan to log requests to the console.
 app.use(methodOverride('_method'));
-app.use(bodyParser.json()); // Give app the ability to parse JSON
+app.use(bodyParser.json()); // Gives app the ability to parse JSON
 app.use(bodyParser.urlencoded({ extended: false })); // Allows app to reqd data from GET requests.
 app.use(cookieParser());
 app.use(express.static(path.join('public')));
